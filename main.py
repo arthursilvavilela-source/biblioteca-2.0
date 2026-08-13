18cc2f70c198ce66f3434ce70f623697760c96b4
Arthur Silva Vilela <arthur.silva.vilela@gmail.com>
2026-08-13 14:19:20 -0300

    cria estrutura inicial do menu com while e if/elif/else so que ainda não tem cadastrar, emprestar

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("Sistema de Geranciamento da Biblioteca")
+def main():
+    programa_rodando = True
+    while programa_rodando:
+        print("\n===== BIBLIOTECA DO SILVA =====")
+        print("1 - Cadastrar livro")
+        print("2 - Sair")
+        opcao = input("Escolha uma opção: ").strip()
+
+        if opcao == "1":
+            print("Você escolheu cadastrar (ainda vamos implementar)")
+        elif opcao == "2":
+            print("Saindo... até mais!")
+            programa_rodando = False
+        else:
+            print("Opção inválida, tente novamente.")
+
+
+if __name__ == "__main__":
+    main()
+         
