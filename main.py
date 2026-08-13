5b98d2a69e8f061667dbb69a048164e86bbdec0e
Arthur Silva Vilela <arthur.silva.vilela@gmail.com>
2026-08-13 15:37:13 -0300

    agora da pra cadrasta os livros mais ainda eles n ficam salvos e falta o resto todo

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
+def cadastrar_livro(livros):
+    print("\n=== Cadastro do novo livro ===")
+    titulo = input("Título do livro: ").strip()
+    ano = input("Ano de publicação: ").strip()
+    autor = input("Nome do autor: ").strip()
+    isbn = input("Código/ISBN: ").strip()
+
+    novo_livro = {
+        "Titulo": titulo,
+        "Ano": ano,
+        "Autor": autor,
+        "ISBN": isbn,
+    }
+    
+    livros.append(novo_livro)
+    print(f"Livro '{titulo}' foi cadastrado com sucesso 👍🏻")
+    return livros
+
+
 def main():
     programa_rodando = True
+    livros = []  # Lista para armazenar os livros cadastrados
+
     while programa_rodando:
         print("\n===== BIBLIOTECA DO SILVA =====")
         print("1 - Cadastrar livro")
@@ -7,7 +28,7 @@ def main():
         opcao = input("Escolha uma opção: ").strip()
 
         if opcao == "1":
-            print("Você escolheu cadastrar (ainda vamos implementar)")
+            livros = cadastrar_livro(livros)
         elif opcao == "2":
             print("Saindo... até mais!")
             programa_rodando = False
@@ -17,4 +38,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-         
